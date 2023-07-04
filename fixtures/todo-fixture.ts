0ba18c50def7769da85578bb140aba4dbdbf1952
Maksym Vasylchuk <mvasylchuk.pro@gmail.com>
2023-07-04 21:00:40 +0300

    minor fix and added tsconfig

--- fixtures/todo-fixture.ts
+++ fixtures/todo-fixture.ts
@@ -1,7 +1,7 @@
 import { test as base} from '@playwright/test';
 import { TodoPage } from '../pages/todo-page';
 
-export const test = base.extend<{ todoPage: TodoPage, a: Boolean}>({
+export const test = base.extend<{ todoPage: TodoPage}>({
     todoPage: async ({page}, use) => {
         const todoPage = new TodoPage(page);
         await todoPage.goto();
